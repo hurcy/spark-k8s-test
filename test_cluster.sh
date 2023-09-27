46531f9e0360ca6d2b7d1e04a255071260210d8a
Cinyoung Hur <cinyoung.hur@gmail.com>
2023-09-28 00:56:47 +0900

    Create test_cluster.sh

---
+++ test_cluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+spark-submit \
+    --master k8s://https://mgmt-lb-1:6443 \
+    --deploy-mode cluster \
+    --name spark-pi \
+    --conf spark.kubernetes.container.image=greyparrotkhc/spark:3.3.2-hadoop3 \
+    pi.py
